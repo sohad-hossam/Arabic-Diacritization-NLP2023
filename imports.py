9d4aad6a4bbdaa22a5b18f25fe46ea7eca7d9f4b
yasminmessycodes <yasminh@silverkeytech.com>
2023-12-24 21:50:19 +0200

    bi-lstm

--- imports.py
+++ imports.py
@@ -9,12 +9,13 @@ from torch.utils.data import Dataset
 from torch.utils.data import DataLoader
 from torch.utils.data import random_split
 from gensim.models import Word2Vec
+import tensorflow as tf
 
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Bidirectional, LSTM, Dense
+from tensorflow.keras.layers import Bidirectional, LSTM, Dense,Embedding
 
 import gensim
 from gensim.models import Word2Vec
-
+import numpy as np
 
 nltk.download('punkt')
